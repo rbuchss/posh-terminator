a53eaad4ef9f5c46e85969f1cc0c4d98b0cf8252
rbuchss <rbuchss@gmail.com>
2020-02-06 14:38:28 -0500

    add bootstrap for windows-terminal profile

---
+++ bootstrap/windows-terminal.ps1
@@ -0,0 +1,13 @@
+<#
+    Symlink profiles.json to windows-terminal config path
+
+    INFO: how to add a relative path symlink:
+        cmd /c mklink /d ".\home\.windows-terminal" "..\windows-terminal"
+#>
+
+$configPath = (Get-Item "~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState")
+$target = (Get-Item "~/.windows-terminal/profiles.json")
+
+Write-Host "New-Item -ItemType SymbolicLink -Path $configPath -Name $($target.Name) -Value $target" `
+    -ForegroundColor Yellow
+New-Item -ItemType SymbolicLink -Path $configPath -Name $target.Name -Value $target -Confirm -Force > $null
