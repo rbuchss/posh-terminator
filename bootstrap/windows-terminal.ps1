a225b842c2ecce144c6af2509a5e6651fa694757
rbuchss <rbuchss@gmail.com>
2020-02-06 17:24:36 -0500

    convert windows-terminal profile to hard-link so changes are live without restart

--- bootstrap/windows-terminal.ps1
+++ bootstrap/windows-terminal.ps1
@@ -8,6 +8,6 @@
 $configPath = (Get-Item "~\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState")
 $target = (Get-Item "~/.windows-terminal/profiles.json")
 
-Write-Host "New-Item -ItemType SymbolicLink -Path $configPath -Name $($target.Name) -Value $target" `
+Write-Host "New-Item -ItemType HardLink -Path $configPath -Name $($target.Name) -Value $target" `
     -ForegroundColor Yellow
-New-Item -ItemType SymbolicLink -Path $configPath -Name $target.Name -Value $target -Confirm -Force > $null
+New-Item -ItemType HardLink -Path $configPath -Name $target.Name -Value $target -Confirm -Force
