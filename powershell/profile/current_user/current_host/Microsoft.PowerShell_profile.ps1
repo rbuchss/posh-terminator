21e6837d5323082e57be14e8d66d0b045e5bd5aa
rbuchss <rbuchss@gmail.com>
2020-02-09 17:49:21 -0500

    add powershell current user current host profile

---
+++ powershell/profile/current_user/current_host/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,69 @@
+<#
+  PowerShell profile docs see:
+    https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7
+    http://akuederle.com/modify-your-powershell-prompt
+    https://www.howtogeek.com/50236/customizing-your-powershell-profile/
+#>
+
+Import-Module posh-git
+
+<# Enviroment Vars #>
+
+# dotnet settings
+$Env:DOTNET_CLI_TELEMETRY_OPTOUT = $true
+
+# PoshGit settings
+#   https://github.com/dahlbyk/posh-git/blob/master/src/GitPrompt.ps1#L899
+$GitPromptSettings.DefaultPromptPath.Text = ''
+$GitPromptSettings.DefaultPromptSuffix.Text = ''
+
+<# Helper functions #>
+
+function Pro { code $PROFILE }
+
+function Get-CmdletAlias ($cmdletname) {
+  Get-Alias |
+    Where-Object -FilterScript {$_.Definition -like "$cmdletname"} |
+      Format-Table -Property Definition, Name -AutoSize
+}
+
+<# Aliases #>
+
+Set-Alias -Name g -Value git
+
+<# Powershell prompt #>
+
+function Test-IsWindows {
+  $IsLinuxEnv = (Get-Variable -Name "IsLinux" -ErrorAction Ignore) -and $IsLinux
+  $IsMacOSEnv = (Get-Variable -Name "IsMacOS" -ErrorAction Ignore) -and $IsMacOS
+  $IsWinEnv = !$IsLinuxEnv -and !$IsMacOSEnv
+  return $IsWinEnv
+}
+
+function Test-Administrator {
+  if (-not (Test-IsWindows)) { return $false }
+  $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+  (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+function global:prompt {
+  $prompt = ''
+
+  if (Test-Administrator) {
+    $prompt += Write-Prompt("PS") -ForegroundColor ([ConsoleColor]::Red)
+  } else {
+    $prompt += Write-Prompt("PS") -ForegroundColor ([ConsoleColor]::DarkCyan)
+  }
+
+  $prompt += Write-Prompt([char]0x222B) -ForegroundColor ([ConsoleColor]::Yellow)
+  $prompt += Write-Prompt([Environment]::UserName) -ForegroundColor ([ConsoleColor]::Blue)
+  $prompt += Write-Prompt("@") -ForegroundColor ([ConsoleColor]::Blue)
+  $prompt += Write-Prompt([Environment]::MachineName.ToLower()) -ForegroundColor ([ConsoleColor]::Red)
+  $prompt += Write-Prompt(" ")
+  $prompt += Write-Prompt($pwd -Replace ($HOME).Replace('\', '\\'), '~') -ForegroundColor ([ConsoleColor]::Yellow)
+  $prompt += & $GitPromptScriptBlock  # PoshGit
+  $prompt += Write-Prompt "`n"
+  $prompt += Write-Prompt([char]0x03BB) -ForegroundColor ([ConsoleColor]::Yellow)
+
+  if ($prompt) { "$prompt " } else { " " }
+}
