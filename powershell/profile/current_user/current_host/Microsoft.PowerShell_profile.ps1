4031e0e730565ee40bc79516fc15eb6480c20c0d
rbuchss <rbuchss@gmail.com>
2020-02-14 12:31:32 -0500

    add search all session history helper

--- powershell/profile/current_user/current_host/Microsoft.PowerShell_profile.ps1
+++ powershell/profile/current_user/current_host/Microsoft.PowerShell_profile.ps1
@@ -57,11 +57,29 @@ function Set-NewLocation {
   Set-Location $dir
 }
 
+function Get-HistoryAllSessions {
+  Get-Content (Get-PSReadlineOption).HistorySavePath
+}
+
+function Find-HistoryAllSessions {
+  if ($args.Count -gt 1) {
+    throw "$($MyInvocation.MyCommand): multiple arguments supplied`nUsage: $($MyInvocation.MyCommand) <regexp pattern>"
+  }
+
+  $find = $args[0]
+  if ($null -eq $find) {
+    throw "$($MyInvocation.MyCommand): No find pattern supplied"
+  }
+
+  Get-HistoryAllSessions | Select-String -Pattern "$find" | Get-Unique
+}
+
 <# Aliases #>
 
 Set-Alias -Name g -Value git
 Set-Alias -Name touch -Value Update-File
 Set-Alias -Name mkcd -Value Set-NewLocation
+Set-Alias -Name hack -Value Find-HistoryAllSessions
 
 <# Powershell prompt #>
 
