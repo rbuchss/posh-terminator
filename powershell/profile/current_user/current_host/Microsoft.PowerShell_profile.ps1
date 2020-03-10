55dbb39310976c34aa84f399cb55a54747c13206
rbuchss <rbuchss@gmail.com>
2020-03-10 15:54:35 -0400

    add dotnet cli tab completion

--- powershell/profile/current_user/current_host/Microsoft.PowerShell_profile.ps1
+++ powershell/profile/current_user/current_host/Microsoft.PowerShell_profile.ps1
@@ -323,6 +323,16 @@ function Get-TypeAliases {
   $tna.GetEnumerator() | Sort-Object Key
 }
 
+<# Auto-Completion #>
+
+# PowerShell parameter completion shim for the dotnet CLI
+Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
+  param($commandName, $wordToComplete, $cursorPosition)
+  dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+  }
+}
+
 <# Aliases #>
 
 Set-Alias -Name g -Value git
