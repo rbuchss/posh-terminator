b70b032f8d3003b6cdc23d8f086426cdebb67ade
rbuchss <rbuchss@gmail.com>
2020-07-01 18:00:30 -0400

    resolve module already loaded test errors in windows

--- test/bin/pester.ps1
+++ test/bin/pester.ps1
@@ -1,4 +1,8 @@
 #!/usr/bin/env pwsh -NoProfile -NonInteractive -NoLogo
 
+if (Get-Module -Name 'posh-terminator') {
+  Remove-Module -Name posh-terminator *>$null
+}
+
 Write-Output 'Invoke-Pester test/ -EnableExit'
 Invoke-Pester test/ -EnableExit
